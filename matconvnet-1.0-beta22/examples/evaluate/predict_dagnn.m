33df0a61c71be99554a82512098dc07f33c06863
jiangtaoxie <jiangtaoxie@mail.dlut.edu.cn>
2017-09-30 10:00:21 +0800

    Revert "MPN-COV"
    This reverts commit 7fc0bc1b596463a90dbc882036c206bc8f968dfa.

--- matconvnet-1.0-beta22/examples/evaluate/predict_dagnn.m
+++
@@ -1,9 +0,0 @@
-function score = predict_dagnn(data,label,net)
-%PREDICT_DAGNN  Predict the input image for DAGNN network
-% Created by Jiangtao Xie
-inputs = {'input', data, 'label', label} ;
-net.eval(inputs);
-prediction_id = net.getVarIndex('prediction');
-score = net.vars(prediction_id).value;
-end
-
--- matconvnet-1.0-beta22/examples/evaluate/predict_simplenn.m
+++
@@ -1,8 +0,0 @@
-function score = predict_simplenn(data,label,net)
-%PREDICT_SIMPLENN Predict the input image for SIMPLENN network
-%  Created by Jiangtao Xie
-net.layers{end}.class = label;
-res = vl_simplenn(net,data,[],[],'mode', 'test');
-score = gather(res(end-1).x);
-end
-
